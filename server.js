b541f1963ced47009dd8922b2bd24c0b5273e6e5
easella <74786054+easella@users.noreply.github.com>
2021-07-05 13:33:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var express=require("express"
-app=express()
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'/index.html')
-}.listen(8080)
+var express=require("express")
+app=express() 
+var Unblocker = require('unblocker'); 
+var unblocker = new Unblocker({prefix: '/main/'}); // this must be one of the first app.use() calls and must not be on a subdirectory to work properly 
+app.use(unblocker); 
+app.get('/',(req,res)=>{ 
+  res.sendFile(__dirname+'/index.html') 
+}).listen(8080)
+app.get('/unblocker',(req,res)=>{ 
+  res.sendFile(__dirname+'/unblocker.html') 
+}).listen(8080)
